443d653e3699b861237ab3dd097ad76438dc9d94
Garfield100 <Garfield100@users.noreply.github.com>
2023-09-14 15:23:57 +0200

    Adjust stream struct fields Now only extracts and exposes mandatory and very common fields

--- src/streams.rs
+++ src/streams.rs
@@ -1,25 +1,43 @@
 use std::collections::HashMap;
+use std::rc::Rc;
 
-use crate::chunk_structs::{Chunk, FileHeaderChunk, Sample, SamplesChunk, StreamFooterChunk, StreamHeaderChunk};
+use crate::chunk_structs::{Chunk, FileHeaderChunk, SamplesChunk, StreamFooterChunk, StreamHeaderChunk};
 use crate::errors::{self, Result};
+use crate::{Format, Sample};
+
+// minimal tags in version 1.x:
+// channel count
+// nominal srate
+// channel format
+
+// common additional tags:
+// name
+// type
+// desc
 
 #[derive(Debug)]
 pub struct Stream {
-    pub stream_id: u32,
-    pub first_timestamp: Option<f64>,
-    pub last_timestamp: Option<f64>,
-    pub sample_count: u64,
-    pub measured_srate: Option<f64>,
-    pub stream_header: xmltree::Element,
+    pub stream_id: u32, // TODO only used internally to match stream headers, footers, and samples
+
+    pub channel_count: u32,
+    pub nominal_srate: Option<f64>, //a mandatory field but we replace zero with None
+    pub format: Format,
+
+    // optional fields
+    pub name: Option<Rc<str>>,
+    pub r#type: Option<Rc<str>>,
+
+    pub stream_header: xmltree::Element, //also contains desc
     pub stream_footer: xmltree::Element,
+
     pub samples: Vec<Sample>,
 }
 
-pub(crate) fn chunks_to_streams(chunks: Vec<Chunk>) -> Result<Vec<Stream>> {
+pub(crate) fn chunks_to_streams(chunks: Vec<Chunk>) -> Result<HashMap<u32, Stream>> {
     let mut file_header_chunk: Option<FileHeaderChunk> = None;
     let mut stream_header_chunks: Vec<StreamHeaderChunk> = Vec::new();
     let mut stream_footer_chunks: Vec<StreamFooterChunk> = Vec::new();
-    let mut samples_chunks: Vec<SamplesChunk> = Vec::new();
+    let mut samples_chunks_map: HashMap<u32, Vec<SamplesChunk>> = HashMap::new();
 
     for chunk in chunks {
         match chunk {
@@ -33,7 +51,7 @@ pub(crate) fn chunks_to_streams(chunks: Vec<Chunk>) -> Result<Vec<Stream>> {
                 stream_footer_chunks.push(c);
             }
             Chunk::SamplesChunk(c) => {
-                samples_chunks.push(c);
+                samples_chunks_map.entry(c.stream_id).or_insert(Vec::new()).push(c);
             }
             _ => {}
         }
@@ -65,28 +83,41 @@ pub(crate) fn chunks_to_streams(chunks: Vec<Chunk>) -> Result<Vec<Stream>> {
     for (&stream_id, stream_header) in &stream_header_map {
         let stream_footer = stream_footer_map.get(&stream_id).unwrap();
 
+        let name = if let Some(name) = &stream_header.info.name {
+            Some(Rc::from(name.as_str()))
+        } else {
+            None
+        };
+
+        let r#type = if let Some(r#type) = &stream_header.info.r#type {
+            Some(Rc::from(r#type.as_str()))
+        } else {
+            None
+        };
+
+        let samples_vec = samples_chunks_map
+            .entry(stream_id)
+            .or_insert(Vec::new())
+            .into_iter()
+            .flat_map(|c| c.samples.clone())
+            .collect();
+        // let samples: &[Sample] = &samples_vec;
+
         let stream = Stream {
             stream_id,
-            first_timestamp: stream_footer.info.first_timestamp,
-            last_timestamp: stream_footer.info.last_timestamp,
-            sample_count: stream_footer.info.sample_count,
-            measured_srate: stream_footer.info.measured_srate,
+            channel_count: stream_header.info.channel_count,
+            nominal_srate: stream_header.info.nominal_srate,
+            format: stream_header.info.channel_format,
+
+            name,
+            r#type,
             stream_header: stream_header.xml.clone(),
             stream_footer: stream_footer.xml.clone(),
-            samples: Vec::new(),
+            samples: samples_vec,
         };
 
         streams_map.insert(stream_id, stream);
     }
 
-    for mut samples_chunk in samples_chunks {
-        let samples_stream_id = samples_chunk.stream_id;
-        let stream = streams_map
-            .get_mut(&samples_stream_id)
-            .ok_or(errors::ErrorKind::MissingStreamHeaderChunk(samples_stream_id))?;
-
-        stream.samples.append(&mut samples_chunk.samples);
-    }
-
-    Ok(streams_map.into_values().collect())
+    Ok(streams_map)
 }
