4ba4a63819f0a4c9528f3e20b6337a171c7cc5d8
Garfield100 <Garfield100@users.noreply.github.com>
2023-09-29 10:49:08 +0200

    minor changes; prepare for branching

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     // println!("{}",std::env::current_dir().unwrap());
     // let reader = fs::File::open("/home/garfield/projects/rust/xdf-rs/example-files/minimal.xdf").unwrap();
-    let bytes = fs::read("/home/garfield/projects/rust/xdf-rs/example-files/tmp/xdf_009.xdf").unwrap();
+    let bytes = fs::read("/home/garfield/projects/rust/xdf-rs/example-files/tmp/xdf_006.xdf").unwrap();
     let xdf_file = XDFFile::from_bytes(&bytes).unwrap();
 
     println!("{:#?}", xdf_file.header);
