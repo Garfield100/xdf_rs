cd7b683df4f5fe0c327c95c589fcf9c00951752b
Garfield100 <Garfield100@users.noreply.github.com>
2023-11-26 21:05:48 +0100

    add large benchmark almost 600M file

--- benches/parse_files.rs
+++ benches/parse_files.rs
@@ -2,15 +2,6 @@ use glassbench::*;
 use xdf;
 
 fn bench_parse_files(bench: &mut Bench) {
-    // load the file into memory
-    // let xdf_001_bytes = std::fs::read("example-files/tmp/xdf_001.xdf").unwrap();
-    // bench.task("xdf_001.xdf - 592M", |task| {
-    //     task.iter(|| {
-    //         let xdf_data = xdf::XDFFile::from_reader(xdf_001_bytes.as_slice()).unwrap();
-    //         pretend_used(&xdf_data);
-    //     });
-    // });
-
     let minimal_bytes = std::fs::read("example-files/minimal.xdf").unwrap();
     bench.task("minimal.xdf - 4.0K", |task| {
         task.iter(|| {
@@ -35,6 +26,13 @@ fn bench_parse_files(bench: &mut Bench) {
         });
     });
 
+    let xdf_001_bytes = std::fs::read("example-files/tmp/xdf_001.xdf").unwrap();
+    bench.task("xdf_001.xdf - 592M", |task| {
+        task.iter(|| {
+            let xdf_data = xdf::XDFFile::from_bytes(xdf_001_bytes.as_slice()).unwrap();
+            pretend_used(&xdf_data);
+        });
+    });
 }
 
 glassbench!("parse_files", bench_parse_files,);
