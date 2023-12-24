492101804386e31f6e91162596153afd10d50103
Garfield100 <Garfield100@users.noreply.github.com>
2023-12-24 12:09:56 +0100

    add util tests

--- src/util.rs
+++ src/util.rs
@@ -35,7 +35,7 @@ pub(crate) fn parse_version(root: &Element) -> Result<f32> {
         }
     };
 
-    return Ok(version);
+    Ok(version)
 }
 
 pub(crate) fn get_text_from_child(root: &Element, child_name: &str) -> Result<String> {
@@ -47,12 +47,11 @@ pub(crate) fn get_text_from_child(root: &Element, child_name: &str) -> Result<St
         .to_string())
 }
 
-// TODO implement using traits instead
 pub(crate) fn opt_string_to_f64(opt_string: Option<String>) -> Result<Option<f64>> {
     opt_string
         .map(|s| s.parse::<f64>())
         .transpose()
-        .map_err(|e| Error::from(e))
+        .map_err(Error::from)
 }
 
 pub(crate) fn extract_timestamp(raw_chunk: &RawChunk, offset: &mut usize) -> Option<f64> {
@@ -70,7 +69,7 @@ pub(crate) fn extract_timestamp(raw_chunk: &RawChunk, offset: &mut usize) -> Opt
         *offset += 1;
     }
 
-    return timestamp;
+    timestamp
 }
 
 #[test]
@@ -98,4 +97,79 @@ fn test_extract_timestamp_some() {
     let timestamp = extract_timestamp(&raw_chunk, &mut offset);
     assert_eq!(timestamp, Some(5.09999999999999964472863211995_f64));
     assert_eq!(offset, 9);
+}
+
+#[test]
+fn test_parse_version() {
+    let mut root = Element::new("root");
+    let mut version_element = Element::new("version");
+    version_element.children.push(xmltree::XMLNode::Text("1.0".to_string()));
+    root.children.push(xmltree::XMLNode::Element(version_element));
+
+    let result = parse_version(&root);
+    assert_eq!(result.unwrap(), 1.0);
+}
+
+#[test]
+fn test_parse_version_missing_tag() {
+    let root = Element::new("root");
+
+    let result = parse_version(&root);
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_parse_version_empty_tag() {
+    let mut root = Element::new("root");
+    let version_element = Element::new("version");
+    root.children.push(xmltree::XMLNode::Element(version_element));
+
+    let result = parse_version(&root);
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_get_text_from_child() {
+    let mut root = Element::new("root");
+    let mut child_element = Element::new("child");
+    let child_text = "value".to_string();
+
+    child_element.children.push(xmltree::XMLNode::Text(child_text.clone()));
+
+    root.children.push(xmltree::XMLNode::Element(child_element));
+
+    let result = get_text_from_child(&root, "child");
+    assert_eq!(result.unwrap(), child_text);
+}
+
+#[test]
+fn test_get_text_from_child_missing_child() {
+    let root = Element::new("root");
+
+    let result = get_text_from_child(&root, "child");
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_get_text_from_child_empty_child() {
+    let mut root = Element::new("root");
+    let child_element = Element::new("child");
+    root.children.push(xmltree::XMLNode::Element(child_element));
+
+    let result = get_text_from_child(&root, "child");
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_opt_string_to_f64_some() {
+    let opt_string = Some("3.14".to_string());
+    let result = opt_string_to_f64(opt_string);
+    assert_eq!(result.unwrap(), Some(3.14));
+}
+
+#[test]
+fn test_opt_string_to_f64_none() {
+    let opt_string = None;
+    let result = opt_string_to_f64(opt_string);
+    assert_eq!(result.unwrap(), None);
 }
