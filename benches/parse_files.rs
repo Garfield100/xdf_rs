fea0e129a290421ae2f2b0299ae8bd78e73423ba
Enrico <5878721+Garfield100@users.noreply.github.com>
2024-04-08 08:56:55 +0200

    comment-out most benchmarks the files used have to be downloaded first and the bigger files have very inconsistent benchmarks

--- benches/parse_files.rs
+++ benches/parse_files.rs
@@ -5,7 +5,7 @@ use xdf;
 // Files used in the tmp folder here can be downloaded from https://osf.io/uc7wn/ (thank you to Clemens Brunner for the upload)
 
 fn bench_parse_files(c: &mut Criterion) {
-    let minimal_bytes = fs::read("example-files/minimal.xdf").unwrap();
+    let minimal_bytes = fs::read("tests/minimal.xdf").unwrap();
     c.bench_function("minimal.xdf - 4.0K", |b| {
         b.iter(|| {
             let xdf_data = xdf::XDFFile::from_bytes(black_box(&minimal_bytes)).unwrap();
@@ -13,29 +13,32 @@ fn bench_parse_files(c: &mut Criterion) {
         });
     });
 
-    let xdf_009_bytes = fs::read("example-files/tmp/xdf_009.xdf").unwrap();
-    c.bench_function("xdf_009.xdf - 7.5M", |b| {
-        b.iter(|| {
-            let xdf_data = xdf::XDFFile::from_bytes(black_box(&xdf_009_bytes)).unwrap();
-            black_box(xdf_data);
-        });
-    });
+    // benchmarks for bigger files which can be downloaded from the link above.
+    // They are much less consistent across trials than the small one, likely for scheduling and i/o reasons.
 
-    let xdf_006_bytes = fs::read("example-files/tmp/xdf_006.xdf").unwrap();
-    c.bench_function("xdf_006.xdf - 62M", |b| {
-        b.iter(|| {
-            let xdf_data = xdf::XDFFile::from_bytes(black_box(&xdf_006_bytes)).unwrap();
-            black_box(xdf_data);
-        });
-    });
+    // let xdf_009_bytes = fs::read("example-files/tmp/xdf_009.xdf").unwrap();
+    // c.bench_function("xdf_009.xdf - 7.5M", |b| {
+    //     b.iter(|| {
+    //         let xdf_data = xdf::XDFFile::from_bytes(black_box(&xdf_009_bytes)).unwrap();
+    //         black_box(xdf_data);
+    //     });
+    // });
 
-    let xdf_001_bytes = fs::read("example-files/tmp/xdf_001.xdf").unwrap();
-    c.bench_function("xdf_001.xdf - 592M", |b| {
-        b.iter(|| {
-            let xdf_data = xdf::XDFFile::from_bytes(black_box(&xdf_001_bytes)).unwrap();
-            black_box(xdf_data);
-        });
-    });
+    // let xdf_006_bytes = fs::read("example-files/tmp/xdf_006.xdf").unwrap();
+    // c.bench_function("xdf_006.xdf - 62M", |b| {
+    //     b.iter(|| {
+    //         let xdf_data = xdf::XDFFile::from_bytes(black_box(&xdf_006_bytes)).unwrap();
+    //         black_box(xdf_data);
+    //     });
+    // });
+
+    // let xdf_001_bytes = fs::read("example-files/tmp/xdf_001.xdf").unwrap();
+    // c.bench_function("xdf_001.xdf - 592M", |b| {
+    //     b.iter(|| {
+    //         let xdf_data = xdf::XDFFile::from_bytes(black_box(&xdf_001_bytes)).unwrap();
+    //         black_box(xdf_data);
+    //     });
+    // });
 }
 
 criterion_group!{
